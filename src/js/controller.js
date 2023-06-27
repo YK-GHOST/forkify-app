23471d01b703baf72ec0e549d34fa997819a60f8
YK-GHOST <yogeshkantiwal20@gmail.com>
2023-06-27 15:51:58 +0530

    Added new Welcome Feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -119,6 +119,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 //Publisher-Subscriber Pattern
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -128,6 +132,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 init();
